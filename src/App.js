fe43c08ccc5be47a0458e782186e421bae866ad5
john7319 <jonniekaras@gmail.com>
2024-05-01 23:12:17 +0300

    Completed nested routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, {useState, useEffect} from "react";
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/users")
+      .then(r => r.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error));
+  }, []);
+  console.log(users)
   return (
     <>
       <header>
-        
+        <NavBar/>
       </header>
+      <Outlet context={users}/>
     </>
   );
 };
